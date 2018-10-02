4f0183c3aeb26b93cb281d31efc32b83965e2cb7
terng03412 <terng_pattarapol@hotmail.co.th>
2018-10-02 18:47:26 +0700

    login phase 1 success

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react';
 import 'semantic-ui-css/semantic.css';
-import { Segment, Button, Divider } from 'semantic-ui-react';
-import Header from '../template/Header';
+import {Container, Button, Form, Grid} from 'semantic-ui-react';
+import Head from '../template/Header';
 import Footer from '../template/Footer';
 
 class LoginContent extends Component {
     render() {
-        return (            
-            <Segment padded>
-                <Button primary fluid>
-                    Login
-                </Button>
-                <Divider horizontal>Or</Divider>
-                <Button secondary fluid>
-                    Sign Up Now
-                </Button>
-            </Segment>
+        return (          
+          <Container>
+            <h1>LOG IN</h1>
+            <Form>
+            
+              <Form.Field>
+                <label>Email</label>
+                <input placeholder='your@email.com' />
+              </Form.Field>
+              
+              <Form.Field>
+                <label>Password</label>
+                <input size="small" type="password" placeholder='password' />
+              </Form.Field>
+    
+
+              <Form.Field>
+                <Button color='blue'>Login</Button>               
+                <a href="#" onclick="console.log('The link was clicked.'); return false"> Forgot your password? </a>   
+              </Form.Field>
+              
+              <Form.Field>
+                <label><h3>New Customer? </h3></label>
+              </Form.Field>
+
+              <Container>
+                <p>
+                Sign up for an account to take advantage of order tracking and history as well as pre-filled forms during checkout on subsequent orders.
+                </p>
+                
+                <Form.Field>
+                <Button>Register</Button>               
+              </Form.Field>
+
+              </Container>
+
+              </Form>
+            
+          </Container>
         );
     }
 }
@@ -24,7 +53,7 @@ export default class Login extends Component {
     render() {
       return (
         <div>
-          <Header/>
+          <Head/>
           <LoginContent/>
           <Footer/>
         </div>
---
+++ src/components/UseCasePage/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import 'semantic-ui-css/semantic.css';
+import {} from 'semantic-ui-react';
+import Header from '../template/Header';
+import Footer from '../template/Footer';
+
+
+class Content extends Component {
+    render() {
+        return (            
+            <h1>Code here</h1>
+        );
+    }
+}
+
+export default class Xx extends Component {
+    render() {
+      return (
+        <div>
+          <Header/>
+          <Content/>
+          <Footer/>
+        </div>
+      );
+    }
+  }
