a392e9ad54a74fd66bf3913f70a3d01dc775d263
chanchai <chanchai.c@ku.th>
2018-10-02 16:38:21 +0700

    add pure template

--- src/components/UseCasePage /index.js
+++ src/components/UseCasePage /index.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-import 'semantic-ui-css/semantic.css';
-import {} from 'semantic-ui-react';
 import Header from '../template/Header';
 import Footer from '../template/Footer';
-
+import 'semantic-ui-css/semantic.css';
+import {} from 'semantic-ui-react';
 
 class Content extends Component {
-    render() {
-        return (            
-            <h1>Code here</h1>
-        );
-    }
+  render() {
+    return (
+      //code here (delete <div/> below)            
+      <div/>
+    );
+  }
 }
 
 export default class Xx extends Component {
-    render() {
-      return (
-        <div>
-          <Header/>
-          <Content/>
-          <Footer/>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Content/>
+        <Footer/>
+      </div>
+    );
   }
+}
