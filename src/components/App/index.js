03bc5ad1af538332193582835f1c5be818f60eba
terng03412 <terng_pattarapol@hotmail.co.th>
2018-10-02 13:34:11 +0700

    add path && split file

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+import './style.css';
 import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+          <h1>Welcome to React</h1>
+        </div>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
