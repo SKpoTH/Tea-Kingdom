7b2593317575e7f49a6253bb59988b8876babdd1
terng03412 <terng_pattarapol@hotmail.co.th>
2018-10-02 14:10:25 +0700

    add semantic login

---
+++ src/components/Login/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import 'semantic-ui-css/semantic.css';
+import { Menu, Container } from 'semantic-ui-react';
+
+class Login extends Component {
+    render() {
+        return (            
+            <h1> Login </h1>
+        );
+    }
+}
+
+export default Login;
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import {Router, Route } from 'react-router';
 
 import App from './components/App';
 import About from './components/About';
+import Login from './components/Login';
 import NotFound from './components/NotFound';
 
 const Routes = (props) => (
@@ -10,6 +11,7 @@ const Routes = (props) => (
         <Route path="/" component={App} />
         <Route path="/about" component={About} />
         <Route path="/notfound" component={NotFound} />
+        <Route path="/login" component={Login} />
     </Router>
 );
 
