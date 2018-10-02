be094d6cf6ceb89cafcc2125dd3de4df4eef7dcf
SKpoTH <skpoxpolice@gmail.com>
2018-10-03 01:11:28 +0700

    Product Page Position update 1

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,15 @@ import React from 'react';
 import {Router, Route } from 'react-router';
 import xx from './components/UseCasePage';
 
+import aa from './components/ProductDetail';
+
+
 const Routes = (props) => (
     <Router {...props}>
         <Route path="/xx" component={xx}/>
+        <Route path="/aa" component={aa}/>
     </Router>
+    
 );
 
 export default Routes;
