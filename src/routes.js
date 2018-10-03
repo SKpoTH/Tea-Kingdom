614414f2a366e36d54b185c933a4110d9bb77b42
SKpoTH <skpoxpolice@gmail.com>
2018-10-03 18:53:52 +0700

    Ready to merge (not modify responsive)

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {Router, Route } from 'react-router';
 import xx from './components/UseCasePage';
 
-import aa from './components/ProductDetail';
+import itemid from './components/ProductDetail';
 
 
 const Routes = (props) => (
     <Router {...props}>
         <Route path="/xx" component={xx}/>
-        <Route path="/aa" component={aa}/>
+        <Route path="/itemid" component={itemid}/>
     </Router>
     
 );
