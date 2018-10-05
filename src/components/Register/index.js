1246ab1558f8b2ead1eb634d1caaba0a73a7afcf
terng03412 <terng_pattarapol@hotmail.co.th>
2018-10-05 10:56:34 +0700

    add path to home at header

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,56 +1,48 @@
-import React, { Component } from 'react';
-import TemplateTKD from '../template/TemplateTKD';
-import 'semantic-ui-css/semantic.css';
-import {Form , Button, Container} from 'semantic-ui-react';
+import React, { Component } from "react";
+import TemplateTKD from "../template/TemplateTKD";
+import "semantic-ui-css/semantic.css";
+import { Form, Button, Checkbox, Container } from "semantic-ui-react";
 
 class Content extends Component {
-    render() {
-        return (
-            <Container>
-            <h1>Register</h1>
-            
-            <Form>
-            
-            <Form.Field>
-                <label>Email</label>
-                <input placeholder='your@email.com' />
-              </Form.Field>
-              
-              
+  render() {
+    return (
+      <Container>
+        <h1>Register</h1>
 
-            <Form.Group unstackable widths={2}>
+        <Form>
+          <Form.Field>
+            <label>Email</label>
+            <input placeholder="your@email.com" />
+          </Form.Field>
+
+          <Form.Group unstackable widths={2}>
             <Form.Field>
-                <label>Password</label>
-                <input type="password" placeholder='password' />
+              <label>Password</label>
+              <input type="password" placeholder="password" />
             </Form.Field>
 
             <Form.Field>
-                <label>Retype Password</label>
-                <input type="password" placeholder='password' />
+              <label>Retype Password</label>
+              <input type="password" placeholder="password" />
             </Form.Field>
-            
-            </Form.Group>
-              
-              
-            <Form.Group unstackable widths={2}>
-              <Form.Input label='First name' placeholder='First name' />
-              <Form.Input label='Last name' placeholder='Last name' />
-            </Form.Group>
-            
-            <Form.Group widths={2}>
-              <Form.Input label='Address' placeholder='Address' />
-              <Form.Input label='Phone' placeholder='Phone' />
-            </Form.Group>
-            
-            <Form.Checkbox label='I agree to the Terms and Conditions' />
-            <Button type='submit'>Submit</Button>
-            </Form> 
-            
+          </Form.Group>
 
-            </Container>           
+          <Form.Group unstackable widths={2}>
+            <Form.Input label="First name" placeholder="First name" />
+            <Form.Input label="Last name" placeholder="Last name" />
+          </Form.Group>
 
-        );
-    }
+          <Form.Group widths={2}>
+            <Form.Input label="Address" placeholder="Address" />
+            <Form.Input label="Phone" placeholder="Phone" />
+          </Form.Group>
+
+          <Form.Checkbox label="I agree to the Terms and Conditions" />
+          <Button type="submit">Submit</Button>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 export default class Register extends Component {
