92576fade23f875de7a48fb4c9beefcee8bac4da
terng03412 <terng_pattarapol@hotmail.co.th>
2018-11-02 17:44:24 +0700

    edit user

--- src/components/EditProfile/index.jsx
+++ src/components/EditProfile/index.jsx
@@ -17,12 +17,10 @@ export default class EditUser extends Component {
     onSubmit = (event) => {
         event.preventDefault();
         const user = {
-            firstname : this.firstname,
-            lastname : this.lastname,
-            email : this.email.value,
-            password : this.password.value,
-            address : this.address,
-            phone : this.phone
+			firstname : this.state.firstname,
+			lastname : this.state.lastname,
+			address : this.state.address,
+			phone : this.state.phone
 		}
 		let checkEmpty = false;
 		for(let a in user) {
@@ -33,19 +31,14 @@ export default class EditUser extends Component {
 		if(checkEmpty) {
 			this.setState( {message : 
 				{ massageHidden : false, content :'You must containt datas in all field.', status: "negative"}});
-		} else if(this.password.value !== this.rePassword.value) {
-			this.setState( {message : 
-				{ massageHidden : false, content :'You password doesn\'t match.', status: "negative"}});
-			this.password.value = "";
-			this.rePassword.value = "";
 		} else if(!this.state.agree) {
 			this.setState( {message : 
 				{ massageHidden : false, content :'You must consider ours agreement.', status: "negative"}});
 		} else {
-			axios.post('/api/signup', user)
+			axios.post('/api/edit_profile/edit', user)
 				.then((res) => {
 					// console.log(res.data);
-					if(res.data.status === "email already used") {
+					if(res.data.status === "Email already used") {
 						this.setState( {message : 
 							{ massageHidden : false, content :res.data.status , status: "negative"}});
 					} else {
@@ -65,26 +58,9 @@ export default class EditUser extends Component {
 		return (
 			<TemplateTKD>
 				<Message content={this.state.message.content} hidden={this.state.message.massageHidden} className={this.state.message.status}/>
-				<h1>Register</h1>
+				<h1>Edit User</h1>
 				<Form onSubmit={ this.onSubmit } >
-					<Form.Field>
-						<label>Email</label>
-						<input type="email" placeholder='your@email.com' ref={(input) => this.email = input} />
-					</Form.Field>
 
-					<Form.Group unstackable widths={2}>
-						<Form.Field>
-							<label>Password</label>
-							<input type="password" placeholder='password' ref={(input) => this.password = input} />
-						</Form.Field>
-
-						<Form.Field>
-							<label>Retype Password</label>
-							<input type="password" placeholder='re-password' ref={(input) => this.rePassword = input} />
-						</Form.Field>
-						
-					</Form.Group>
-					
 					<Form.Group unstackable widths={2}>
 						<Form.Input label='firstname' placeholder='First name' onChange={(e,data)=>{ this.firstname = data.value }} />
 						<Form.Input label='lastname' placeholder='Last name' onChange={(e,data)=>{ this.lastname = data.value }} />
@@ -94,21 +70,8 @@ export default class EditUser extends Component {
 						<Form.Input label='address' placeholder='Address' onChange={(e,data)=>{ this.address = data.value }} />
 						<Form.Input label='phone' placeholder='Phone' onChange={(e,data)=>{ this.phone = data.value }} />
 					</Form.Group>
-					<Form.Group inline>
-					<Form.Field control={Checkbox}onChange={() => {this.setState({agree : !this.state.agree})}}/>
-						I agree to the&nbsp;
-						<Modal trigger={<a>Terms and Conditions</a>}>
-							<Modal.Header>ข้อตกลง</Modal.Header>
-							<Modal.Content>
-								<p>Tea kingdom, through its Website “www.Tea-kingdom.com”, or any other domain or subdomain linked to it and owned by Tea kingdom (hereinafter, the TEA SHOP Website), may request the User for certain personal data. The data provided by the User will be stored in an automated file of personal data, property of Tea kingdom, which will be processed in order to provide the services requested by the User. The User authorizes the automated processing of the personal data provided, necessary for the provision of the services agreed.
 
-								The personal information collected from registered users is stored in a database owned by Tea kingdom, which takes all the necessary technical, organizational and security measures to ensure the confidentiality and integrity of the information in accordance with the provisions of Basic Law , on Personal Data Protection, and other applicable legislation.
-								
-								Any registered User may, at any time, exercise the rights of access, rectification, cancellation and opposition with regard to his personal data provided to the TEA SHOP website, by written communication addressed to Tea kingdom, this communication may also be made by sending an email to the following address: tea-kingdom@teashop.com.</p>
-								
-							</Modal.Content>
-						</Modal>
-					</Form.Group>
+					
 					<Button type='submit'>Submit</Button>
 				</Form>
 			</TemplateTKD>   
