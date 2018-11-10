0bed0716a96b0ea0d08f9f3eeaad4629401335d9
terng03412 <terng_pattarapol@hotmail.co.th>
2018-11-10 18:47:46 +0700

    delete forget passwd

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -5,69 +5,77 @@ import axios from "axios";
 import TemplateTKD from "../template/TemplateTKD";
 
 export default class Login extends Component {
-  constructor(props){
+  constructor(props) {
     // console.log(props.params.Semail);
     super(props);
-        this.state = {
-          message : 
-			      { massageHidden : true, content :'', status: ""},
-          email : "",
-          password : "",
-          eamilRe : props.params.Semail
+    this.state = {
+      message:
+        { massageHidden: true, content: '', status: "" },
+      email: "",
+      password: "",
+      eamilRe: props.params.Semail
     }
-    if(this.state.eamilRe !== undefined) {
-      this.state.message = { massageHidden : false, content :"Register Success : "+this.state.eamilRe, status: "success"};
+    if (this.state.eamilRe !== undefined) {
+      this.state.message = { massageHidden: false, content: "Register Success : " + this.state.eamilRe, status: "success" };
     }
   }
 
   onLoginSubmit = (e) => {
     e.preventDefault();
-    
+
     const Account = {
-      email : this.email.value,
-      password : this.password.value,
+      email: this.email.value,
+      password: this.password.value,
     }
     let checkEmpty = false;
-    for(let a in Account) {
+    for (let a in Account) {
       console.log(Account[a]);
-			if(Account[a] === "" || Account[a] === undefined) {
-				checkEmpty = true;
-			}
-		}
-    if(checkEmpty) {
-			this.setState( {message : 
-        { massageHidden : false, content :'You must containt datas in all field.', status: "negative"}});
-		} else {
-			axios.post('/api/login', Account)
-				.then((res) => {
+      if (Account[a] === "" || Account[a] === undefined) {
+        checkEmpty = true;
+      }
+    }
+    if (checkEmpty) {
+      this.setState({
+        message:
+          { massageHidden: false, content: 'You must containt datas in all field.', status: "negative" }
+      });
+    } else {
+      axios.post('/api/login', Account)
+        .then((res) => {
           console.log(res.data);
-					if(res.data.status === "User and Password are not matched") {
-						this.setState( {message : 
-              { massageHidden : false, content :res.data.status , status: "negative"}});
-              this.password.value = "";
-              this.email.value = "";
-					} else {
+          if (res.data.status === "User and Password are not matched") {
+            this.setState({
+              message:
+                { massageHidden: false, content: res.data.status, status: "negative" }
+            });
+            this.password.value = "";
+            this.email.value = "";
+          } else {
             localStorage.setItem('token', res.data.token);//////////////////
             window.location = '/';
-					}
-				})
-				.catch((error) => {
-					this.setState( {message : 
-            { massageHidden : false, 
-              content :"Error : "+error.response.status+" => "+error.response.data.split("<pre>")[1].split("</pre>")[0], 
-              status: "negative"}}
-            );
-				});
-		}    
+          }
+        })
+        .catch((error) => {
+          this.setState({
+            message:
+            {
+              massageHidden: false,
+              content: "Error : " + error.response.status + " => " + error.response.data.split("<pre>")[1].split("</pre>")[0],
+              status: "negative"
+            }
+          }
+          );
+        });
+    }
   }
 
   render() {
     return (
       <TemplateTKD>
         <Container>
-          <Message content={this.state.message.content} hidden={this.state.message.massageHidden} className={this.state.message.status}/>
+          <Message content={this.state.message.content} hidden={this.state.message.massageHidden} className={this.state.message.status} />
           <h1>LOG IN</h1>
-          <Form onSubmit={ this.onLoginSubmit } >
+          <Form onSubmit={this.onLoginSubmit} >
             <Form.Field>
               <label>Email</label>
               <input type="email" placeholder='your@email.com' ref={(input) => this.email = input} />
@@ -79,11 +87,6 @@ export default class Login extends Component {
             </Form.Field>
             <Button color="blue" >Login</Button>
 
-            <a>
-              {" "}
-              Forgot your password?{" "}
-            </a>
-
             <h3>New Customer? </h3>
 
             <p>
@@ -92,7 +95,7 @@ export default class Login extends Component {
               orders.
             </p>
           </Form>
-          <Button onClick={() => {window.location = '/register'}}>Register</Button>
+          <Button onClick={() => { window.location = '/register' }}>Register</Button>
         </Container>
       </TemplateTKD>
     );
