fa650712146224450c63cc06b3a146b8884a9321
chanchai <pom51142@gmail.com>
2018-11-19 16:32:59 +0700

    2018-11-19_16:32:59

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
-#!/bin/bash 
-npm run-script build 
-rsync -av ./server/build/uploads/* ./build/uploads/ 
-rm -r ./server/build 
-mv ./build ./server/build 
-git add -A 
-git add deploy.sh 
-DATE_WITH_TIME=`date +%Y-%m-%d_%H:%M:%S` 
-git commit -m $DATE_WITH_TIME 
+#!/bin/bash  
+# npm run-script build 
+# rsync -av server/build/uploads/ ./build/uploads/ -dest
+# rm -rf ./server/build/ 
+# rsync -av build/ ./server/build/ -dest
+git add -A && git commit -m `date +%Y-%m-%d_%H:%M:%S`
