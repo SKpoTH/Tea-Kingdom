9a8953b19e9690d5e7a6ed610aea2b2ed671eb35
chanchai <pom51142@gmail.com>
2018-11-19 16:41:25 +0700

    2018-11-19_16:41:25

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash  
-# npm run-script build 
-# rsync -av server/build/uploads/ ./build/uploads/ -dest
-# rm -rf ./server/build/ 
-# rsync -av build/ ./server/build/ -dest
+npm run-script build 
+rsync -av server/build/uploads/ ./build/uploads/ -dest
+rm -rf ./server/build/ 
+rsync -av build/ ./server/build/ -dest
 git add -A && git commit -m `date +%Y-%m-%d_%H:%M:%S`
