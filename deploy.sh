acb7553ba9e9ac25246457f42ec1b090f68f098c
chanchai <pom51142@gmail.com>
2018-11-19 17:43:10 +0700

    Auto Deploy 2018-11-19_17:43:10

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash  
-# npm run-script build 
-# rsync -av server/build/uploads/ ./build/uploads/ -dest
-# rm -rf ./server/build/ 
-# rsync -av build/ ./server/build/ -dest
-# rm -rf ./build/ 
+npm run-script build 
+rsync -av server/build/uploads/ ./build/uploads/ -dest
+rm -rf ./server/build/ 
+rsync -av build/ ./server/build/ -dest
+rm -rf ./build/ 
 git add -A && git commit -m "Auto Deploy "`date +%Y-%m-%d_%H:%M:%S`
-# git push --force heroku master
-# printf " _____                    _  ___                 _                 \n|_   _|__  __ _          | |/ (_)_ __   __ _  __| | ___  _ __ ___  \n  | |/ _ \\/ _\` |  _____  | ' /| | '_ \\ / _\` |/ _\` |/ _ \\| '_ \` _ \\ \n  | |  __/ (_| | |_____| | . \\| | | | | (_| | (_| | (_) | | | | | |\n  |_|\\___|\\__,_|         |_|\\_\\_|_| |_|\\__, |\\__,_|\\___/|_| |_| |_|\n                                       |___/                       \n"
+git push --force heroku master
+printf " _____                    _  ___                 _                 \n|_   _|__  __ _          | |/ (_)_ __   __ _  __| | ___  _ __ ___  \n  | |/ _ \\/ _\` |  _____  | ' /| | '_ \\ / _\` |/ _\` |/ _ \\| '_ \` _ \\ \n  | |  __/ (_| | |_____| | . \\| | | | | (_| | (_| | (_) | | | | | |\n  |_|\\___|\\__,_|         |_|\\_\\_|_| |_|\\__, |\\__,_|\\___/|_| |_| |_|\n                                       |___/                       \n"
