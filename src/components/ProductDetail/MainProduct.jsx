2da9c8c1082755012a1034c482302720ebbd6af3
SKpoTH <skpoxpolice@gmail.com>
2018-12-02 18:56:32 +0700

    Auto Deploy 2018-12-02_18:56:32

--- src/components/ProductDetail/MainProduct.jsx
+++ src/components/ProductDetail/MainProduct.jsx
@@ -150,7 +150,7 @@ export default class MainProduct extends Component {
         <br />
       </React.Fragment>
     );
-    const oostock = amount < 0 ? <Dimmer inverted active><h1 class="frontOutof">Out of stock</h1></Dimmer> : null;
+    const oostock = amount <= 0 ? <Dimmer inverted active><h1 class="frontOutof">Out of stock</h1></Dimmer> : null;
     return (
       <Container>
         <Segment>
